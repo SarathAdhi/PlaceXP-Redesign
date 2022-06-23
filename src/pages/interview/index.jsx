f0796b4f158e7ce311d33ed553dfa0425cdb4361
SarathAdhi <s.sarathadhithya@gmail.com>
2022-06-23 11:55:15 +0530

    minor changes

--- src/pages/interview/index.jsx
+++ src/pages/interview/index.jsx
@@ -52,7 +52,7 @@ export default function Interviews() {
               <LinkedItem
                 key={interview._id}
                 href={`/interview/${interview._id}`}
-                className="w-96 flex flex-col gap-2 bg-white p-2 text-black rounded-lg"
+                className="w-96 flex flex-col gap-2 bg-white p-2 text-black rounded-lg border-[2px] border-primary-200 duration-300 shadow-md hover:shadow-primary-600"
               >
                 <div className="h-3/4 py-3 px-4 flex flex-col gap-y-5 justify-around bg-primary-100 rounded-lg text-center">
                   <H4 className="font-semibold">{interview.postTitle}</H4>
