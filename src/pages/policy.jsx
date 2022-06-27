5c48ee215678dcd041cfd5f7899c0363a083adac
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2022-06-27 10:41:53 +0530

    Update policy.jsx

--- src/pages/policy.jsx
+++ src/pages/policy.jsx
@@ -15,52 +15,52 @@ export default function Policy() {
       <div className="text-justify mb-5">
         <H4 className="font-medium">Accepting The Terms:</H4>
         <ol className="list-decimal my-2 ml-5">
-          <li><P className="">In order to use the Service, you must firstly agree to the Terms. You may not use the Service if you do not accept the Terms.</P></li>
-          <li><P className="">You can accept the Terms by simply using the Service. You understand and agree that PlaceXP will treat your use of the Service as acceptance of the Terms from that point onwards.</P></li>
-          <li><P className="">{"You may not use the Service and may not accept the Terms if (a) you are not of legal age to form a binding contract with PlaceXP, or (b) you are a person who is either barred or otherwise legally prohibited from receiving or using the Service under the laws of the country in which you are resident or from which you access or use the Service."}</P></li>
+          <li><P className="">{"In order to use the Service, you must firstly agree to the Terms. You may not use the Service if you do not accept the Terms."}</P></li>
+          < li > <P className="">{"You can accept the Terms by simply using the Service. You understand and agree that PlaceXP will treat your use of the Service as acceptance of the Terms from that point onwards."}</P></li>
+          < li > <P className="">{"You may not use the Service and may not accept the Terms if (a) you are not of legal age to form a binding contract with PlaceXP, or (b) you are a person who is either barred or otherwise legally prohibited from receiving or using the Service under the laws of the country in which you are resident or from which you access or use the Service."}</P></li>
         </ol>
       </div>
 
       <div className="text-justify mb-5">
         <H4 className="font-medium">Who can use PlaceXP:</H4>
-        <P className="">Our platform is not just open and as inclusive as possible, but also is safe, secure and in accordance with VIT. Our website would be highly beneficial if you are-</P>
-        <ol className="list-decimal my-2 ml-5">
-          <li><P className="">A college student</P></li>
-          <li><P className="">A person in the field of Engineering</P></li>
-          <li><P className="">Wants exponential growth in your career and life</P></li>
+        <P className="">{"Our platform is not just open and as inclusive as possible, but also is safe, secure and in accordance with VIT. Our website would be highly beneficial if you are-"}</P>
+        < ol className="list-decimal my-2 ml-5">
+          <li><P className="">{"A college student"}</P></li>
+          < li ><P className="">{"A person in the field of Engineering"}</P></li>
+          <li><P className="">{"Wants exponential growth in your career and life"}</P></li>
         </ol>
       </div>
 
       <div className="text-justify mb-5">
         <H4 className="font-medium">Who can't use PlaceXP:</H4>
-        <P className="">You can’t use this site if-</P>
+        <P className="">{"You can’t use this site if-"}</P>
         <ol className="list-decimal my-2 ml-5">
-          <li><P className="">You misuse our data.</P></li>
-          <li><P className="">We have prohibited you from using our site.</P></li>
-          <li><P className="">You have violated our community guidelines.</P></li>
-          <li><P className="">You don’t agree with our policies.</P></li>
-          <li><P className="">You attempt to create accounts or access or collect information in unauthorized ways.</P></li>
+          <li><P className="">{"You misuse our data."}</P></li>
+          <li><P className="">{"We have prohibited you from using our site."}</P></li>
+          <li><P className="">{"You have violated our community guidelines."}</P></li>
+          <li><P className="">{"You don’t agree with our policies."}</P></li>
+          <li><P className="">{"You attempt to create accounts or access or collect information in unauthorized ways."}</P></li>
         </ol>
       </div>
 
       <div className="text-justify mb-5">
         <H4 className="font-medium">Your Responsibilities:</H4>
-        <P className="">Hereby I confirm that I won’t use this platform to-</P>
+        <P className="">{"Hereby I confirm that I won’t use this platform to-"}</P>
         <ol className="list-decimal my-2 ml-5">
-          <li><P className="">Obtain unauthorised access to the services.</P></li>
-          <li><P className="">Make available any threatening, harassing, abusing, obscene or hateful content.</P></li>
-          <li><P className="">Invade anyone’s privacy.</P></li>
-          <li><P className="">Violate any laws or regulations.</P></li>
-          <li><P className="">Make available any sort of plagiarised content.</P></li>
-          <li><P className="">Make available viruses or unwanted files.</P></li>
-          <li><P className="">Interfere with or disrupt our services.</P></li>
+          <li><P className="">{"Obtain unauthorised access to the services."}</P></li>
+          <li><P className="">{"Make available any threatening, harassing, abusing, obscene or hateful content."}</P></li>
+          <li><P className="">{"Invade anyone’s privacy."}</P></li>
+          <li><P className="">{"Violate any laws or regulations."}</P></li>
+          <li><P className="">{"Make available any sort of plagiarised content."}</P></li>
+          <li><P className="">{"Make available viruses or unwanted files."}</P></li>
+          <li><P className="">{"Interfere with or disrupt our services."}</P></li>
         </ol>
       </div>
 
       <div className="text-justify mb-5">
         <H4 className="font-medium">The Content:</H4>
-        <P className="">The PlaceXP Site includes a combination of content that we write or that is created and posted by our creative team. All of the content on the PlaceXP Site is protected by the copyright of PlaceXP, other content providers or our users. Materials uploaded to PlaceXP may be subject to posted limitations on usage, reproduction and/or dissemination.</P>
-        <P className="">As a user of the PlaceXP Site, you may not modify, publish, transmit. participate in the transfer or sale of, reproduce, create derivative works of, distribute, publicly perform, publicly display, or in any way exploit any of the content on the PlaceXP Site in whole or in part outside of the specific usage rights granted to you. If you download or print a copy of any PlaceXP content for personal use, you must retain all copyright and other proprietary notices contained therein. You may not otherwise use, reproduce, display, publicly perform, or distribute such content in any way for any public or commercial purpose unless such use is expressly granted by a particular license.</P>
+        <P className="">{"The PlaceXP Site includes a combination of content that we write or that is created and posted by our creative team. All of the content on the PlaceXP Site is protected by the copyright of PlaceXP, other content providers or our users. Materials uploaded to PlaceXP may be subject to posted limitations on usage, reproduction and/or dissemination."}</P>
+        <P className="">{"As a user of the PlaceXP Site, you may not modify, publish, transmit. participate in the transfer or sale of, reproduce, create derivative works of, distribute, publicly perform, publicly display, or in any way exploit any of the content on the PlaceXP Site in whole or in part outside of the specific usage rights granted to you. If you download or print a copy of any PlaceXP content for personal use, you must retain all copyright and other proprietary notices contained therein. You may not otherwise use, reproduce, display, publicly perform, or distribute such content in any way for any public or commercial purpose unless such use is expressly granted by a particular license."}</P>
       </div>
 
       <div className="text-justify mb-5">
@@ -70,29 +70,29 @@ export default function Policy() {
 
       <div className="text-justify mb-5">
         <H4 className="font-medium">Termination of Service:</H4>
-        <P className="">PlaceXP may terminate this Agreement with immediate effect, without prior notice to the Member/User and without further prejudice to all other rights:</P>
+        <P className="">{"PlaceXP may terminate this Agreement with immediate effect, without prior notice to the Member/User and without further prejudice to all other rights:"}</P>
         <ol className="list-decimal my-2 ml-5">
-          <li><P className="">If in the opinion of PlaceXP, the Member has breached any of the terms or conditions of this agreement.</P></li>
-          <li><P className="">If in the opinion of PlaceXP or any regulatory authority, it is not in the public interest to continue providing the Service to the Member for any reason.</P></li>
+          <li><P className="">{"If in the opinion of PlaceXP, the Member has breached any of the terms or conditions of this agreement."}</P></li>
+          <li><P className="">{"If in the opinion of PlaceXP or any regulatory authority, it is not in the public interest to continue providing the Service to the Member for any reason."}</P></li>
         </ol>
-        <P className="">The following could be deemed as the reason for the termination of the services:</P>
+        <P className="">{"The following could be deemed as the reason for the termination of the services:"}</P>
         <ol className="list-decimal my-2 ml-5">
-          <li><P className="">harasses or advocates harassment of another person</P></li>
-          <li><P className="">is patently offensive to the online community, any such content that promotes racism, bigotry, hatred or physical harm of any kind against any group or individual</P></li>
-          <li><P className="">involves unsolicited mass mailing or "spamming", the transmission of "junk mail", "chain letters": promotes any information that is misleading, is false or that promotes illegal activities or conduct that is abusive, threatening, obscene, defamatory or libellous</P></li>
-          <li><P className="">promotes unauthorized or an illegal copy of another person's copyrighted work, such as providing pirated computer programs or links to them, providing information to circumvent manufacture-installed copy-protect devices</P></li>
+          <li><P className="">{"harasses or advocates harassment of another person"}</P></li>
+          <li><P className="">{"is patently offensive to the online community, any such content that promotes racism, bigotry, hatred or physical harm of any kind against any group or individual"}</P></li>
+          <li><P className="">{"involves unsolicited mass mailing or 'spamming', the transmission of 'junk mail', 'chain letters': promotes any information that is misleading, is false or that promotes illegal activities or conduct that is abusive, threatening, obscene, defamatory or libellous"}</P></li>
+          <li><P className="">{"promotes unauthorized or an illegal copy of another person's copyrighted work, such as providing pirated computer programs or links to them, providing information to circumvent manufacture-installed copy-protect devices"}</P></li>
           <li><P className="">{"contains restricted or password only access pages, or hidden pages or images (those not linked to or from another accessible page)"}</P></li>
-          <li><P className="">provides any kind of instructional information about illegal activities such as violating someone's privacy, making or buying illegal weapons, or providing or creating computer viruses, or hacking</P></li>
-          <li><P className="">solicits passwords or personal identifying information for commercial or unlawful purposes from other users</P></li>
+          <li><P className="">{"provides any kind of instructional information about illegal activities such as violating someone's privacy, making or buying illegal weapons, or providing or creating computer viruses, or hacking"}</P></li>
+          <li><P className="">{"solicits passwords or personal identifying information for commercial or unlawful purposes from other users"}</P></li>
         </ol>
-        <P className="font-bold">Any kind of misbehaviour mentioned above, can lead to account suspension and might also lead to the involvement of VIT management.</P>
+        <P className="font-bold">{"Any kind of misbehaviour mentioned above, can lead to account suspension and might also lead to the involvement of VIT management."}</P>
       </div>
 
       <div className="text-justify mb-5">
         <H4 className="font-medium">Updating These Terms</H4>
         <P className="">{"We may change our Service and policies, and we may need to make changes to these Terms so that they accurately reflect our Service and policies. Unless otherwise required by law, we will notify you (for example, through our Service) before we make changes to these Terms and give you an opportunity to review them before they go into effect. Then, if you continue to use the Service, you will be bound by the updated Terms. If you do not want to agree to these or any updated Terms, you can delete your account, here."}</P>
-        <P className="font-bold my-2">Thank you for taking the time to read this Terms Of Use. By following these Terms, the experience will be better for all users.</P>
-        <P className="font-bold my-2">Enjoy PlaceXP Services!</P>
+        <P className="font-bold my-2">{"Thank you for taking the time to read this Terms Of Use. By following these Terms, the experience will be better for all users."}</P>
+        <P className="font-bold my-2">{"Enjoy PlaceXP Services!"}</P>
       </div>
     </PageLayout>
   );
