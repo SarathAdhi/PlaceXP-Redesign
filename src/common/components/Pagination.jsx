987b2cdfa0c80dde050f1d345c8d56c9ddc9ca0f
SarathAdhi <s.sarathadhithya@gmail.com>
2022-07-11 21:54:26 +0530

    pagination for mobile devices

--- src/common/components/Pagination.jsx
+++ src/common/components/Pagination.jsx
@@ -15,13 +15,13 @@ export const Pagination = ({ Component, page, length }) => {
       <div className="w-full bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
         <div className="flex-1 flex justify-between sm:hidden">
           <LinkedItem
-            href="#"
+            href={page === 1 ? `?page=${page}` : `?page=${page - 1}`}
             className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Previous
           </LinkedItem>
           <LinkedItem
-            href="#"
+            href={page >= length ? `?page=${page}` : `?page=${page + 1}`}
             className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Next
