0d02d819b3736e443dae7463c6235505f1a6445b
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2022-08-18 22:26:08 +0530

    Changed Card details in home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,24 +11,27 @@ const cardsDetails = [
     img: "https://img.icons8.com/external-icongeek26-outline-icongeek26/64/undefined/external-interview-office-icongeek26-outline-icongeek26.png",
     text: "Nervous about interviews? Worry no more because we have here a conglomeration of interview experiences of our seniors at various prestigous institutions.",
     button: "Explore Interviews",
+    link: "/interviews"
   },
   {
     title: "Hackathons",
     img: "https://img.icons8.com/wired/64/undefined/code.png",
     text: "Wanna feel the rush behind coding competitions? Register ASAP and start participating in all the hackathons conducted by well-known organisations. ",
     button: "Explore Hackathons",
+    link: "/hackathons"
   },
-  {
-    title: "Mock Interviews",
-    img: "https://img.icons8.com/ios/50/undefined/document-1.png",
-    text: "Worried about cracking the aptitude test in your interviews? Fret not because PlaceXP has come up with a detailed collection of tests conducted by various prestigious institutions.",
-    button: "Explore Mock Interviews",
-  },
+  // {
+  //   title: "Mock Interviews",
+  //   img: "https://img.icons8.com/ios/50/undefined/document-1.png",
+  //   text: "Worried about cracking the aptitude test in your interviews? Fret not because PlaceXP has come up with a detailed collection of tests conducted by various prestigious institutions.",
+  //   button: "Explore Mock Interviews",
+  // },
   {
     title: "Club Events",
     img: "https://img.icons8.com/external-sbts2018-mixed-sbts2018/58/000000/external-events-social-media-basic-1-sbts2018-mixed-sbts2018.png",
     text: "Missing out on a lot of events conducted at VITC? Don't worry, we have the collection of all the club events that will be taking place at our campus for you to participate.",
     button: "Explore Club Events",
+    link: "/club-events"
   },
 ];
 
